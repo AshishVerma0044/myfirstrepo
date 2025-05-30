cb8a82744ba7827c3ff2a359530921155eb2a751
AshishVerma0044 <ashishv5775@gmail.com>
2025-05-31 00:46:30 +0530

    this function show my name

---
+++ javacode.java
@@ -0,0 +1,16 @@
+public class javacode {
+
+    // Function to print the name
+    public static void printName() {
+        System.out.println("Name: John Doe");
+    }
+
+   
+
+    // Main function
+    public static void main(String[] args) {
+        // Calling the functions
+        printName();
+        printAddress();
+    }
+}
